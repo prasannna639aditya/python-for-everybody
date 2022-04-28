3c51a270ca5a9466c1b773cdc6c180167d99d645
pam03 <18656602+prasannna639aditya@users.noreply.github.com>
2022-04-28 21:50:17 +0530

    Add files via upload

---
+++ Chapter 3/exercise1.py
@@ -0,0 +1,10 @@
+"""Rewrite your pay computation to give the employee 1.5
+times the hourly rate for hours worked above 40 hours.
+Enter Hours: 45
+Enter Rate: 10
+Pay: 475.0"""
+#print("Enter Hours:")
+Hours=int(input('Enter Hours:'))
+Rate=float(input('Enter Rate:'))
+Pay=Hours*Rate
+print("Pay:",Pay)
